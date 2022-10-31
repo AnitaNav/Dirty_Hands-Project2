61278a2256d06741aaa70d103f4671a96d838fb1
AnitaNav <anitakm215@gmail.com>
2022-10-31 11:06:04 -0700

    first model

--- models/plant.js
+++ models/plant.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const plantSchema = new Schema({
-    name: String,
-    
+    name: {
+        type: String
+    },
+    variety: {
+        type: String,
+        enum: ['Herb', 'Shrub','Tree','Succulent','Climber','Creeper']
+    },
+    area: {
+        type: String,
+        enum: ['Indoor','Outdoor']
+    },
+    cycle: {
+        type: String,
+        enum: ['Annual','Perennial', 'Biennial']
+    },
+
+}, {
+    timestamps: true
 });
+module.exports = mongoose.model('Plant', plantSchema);
