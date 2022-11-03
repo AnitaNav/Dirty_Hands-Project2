1028bc4cd79c4b529b697a7900a5f29554d4b9a0
AnitaNav <anitakm215@gmail.com>
2022-11-03 13:06:17 -0700

    Deploy to Heroku

--- models/plant.js
+++ models/plant.js
@@ -9,6 +9,12 @@ const commentSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
+    },
+    userName: {
+      type: String,
+    },
+    userAvatar: {
+      type: String
     }
 }, {
     timestamps: true
